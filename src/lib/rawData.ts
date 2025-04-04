135c556fd246c2239770ea8d2fa517cae19306d5
MobiFone7 <duong.tranvan@mobifone.vn>
2025-04-04 15:20:07 +0700

    up

---
+++ src/lib/rawData.ts
@@ -0,0 +1,122 @@
+export const kpiData = [
+  {
+    nameKpi: "DTHU_TKC_HTS",
+    point: 15,
+    maxPercent: 120,
+  },
+  {
+    nameKpi: "DTHU_FIBER",
+    point: 5,
+    maxPercent: 120,
+  },
+  {
+    nameKpi: "DTHU_MASS",
+    point: 5,
+    maxPercent: 120,
+  },
+  {
+    nameKpi: "DTHU_DUAN",
+    point: 5,
+    maxPercent: 120,
+  },
+  {
+    nameKpi: "DTHU_NDS",
+    point: 5,
+    maxPercent: 120,
+  },
+  {
+    nameKpi: "DTHU_SAYMEE",
+    point: 5,
+    maxPercent: 120,
+  },
+  {
+    nameKpi: "DTHU_GPS",
+    point: 5,
+    maxPercent: 120,
+  },
+  {
+    nameKpi: "SL_C2C",
+    point: 2,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "SL_TB_C2C",
+    point: 4,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TYLE_GD_C2C",
+    point: 2,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "SL_PTM_TBTT_HTS",
+    point: 4,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "SL_PTM_TBTT_NDS",
+    point: 5,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "SL_TBTS_PTM_THOAI",
+    point: 3,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "SL_TB_PTM_M2M",
+    point: 3,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TB_PTM_SAYMEE",
+    point: 3,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TB_PTM_FIBER",
+    point: 3,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TB_VLR",
+    point: 2,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TB_PSC",
+    point: 2,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TB_PLAT_TT",
+    point: 1,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TILE_N_1_GOI",
+    point: 3,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TILE_N_1_DONKY",
+    point: 1.5,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TILE_N_1_DAIKY",
+    point: 1.5,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TILE_MNP",
+    point: 1,
+    maxPercent: 105,
+  },
+  {
+    nameKpi: "TI_LE_DN_SU_DUNG_GP_MBF",
+    point: 1,
+    maxPercent: 105,
+  },
+];
